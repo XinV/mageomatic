821ae3592c29d6910741f1eda650c5942574e295
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-08 14:04:53 -0500

    dev

---
+++ SalsifyConnect/app/code/local/Salsify/Connect/Helper/Downloader.php
@@ -0,0 +1,31 @@
+<?php
+
+// TODO clear out temp directory
+// TODO save file location in data model?
+
+/**
+ * Helper class that takes care of downloading a data dump from Salsify to a
+ * local Magento directory.
+ */
+class Salsify_Downloader {
+
+
+  public function __construct() {
+    // FIXME accept an API token
+  }
+
+  /**
+   * Returns the path to the locally downloaded file.
+   */
+  public function download() {
+    // FIXME
+    return $this->_ensure_temp_directory();
+  }
+
+  private function _ensure_temp_directory() {
+    $magento_root = dirname(dirname(__FILE__)).'/../../../../../../'
+    return $magento_root;
+  }
+
+
+}
