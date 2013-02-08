102a6753dc105aa80b6da8de97d2e713691de751
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-08 14:07:09 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/IndexController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/IndexController.php
@@ -7,7 +7,7 @@ class Salsify_Connect_IndexController extends Mage_Core_Controller_Front_Action 
   }
 
   public function testAction() {
-    $downloader = Mage::helper('salsify_connect/downloader')
+    $downloader = Mage::helper('salsify_connect/downloader');
     echo 'Magento root: ' . $downloader->download();
   }
 
