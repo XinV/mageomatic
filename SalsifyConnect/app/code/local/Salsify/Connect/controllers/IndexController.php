8f281e71465594cc780f92a2f06f06f4e6914fc3
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-11 17:14:58 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/IndexController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/IndexController.php
@@ -15,15 +15,10 @@ class Salsify_Connect_IndexController extends Mage_Core_Controller_Front_Action 
   }
 
   public function testloadAction() {
-    $downloader = Mage::helper('salsify_connect/downloader');
-    $file = $downloader->download();
-    echo '<br/>Temp file for uploading: ' . $file;
-
-    echo '<br/>Getting helper...';
     $salsify = Mage::helper('salsify_connect');
 
     echo '<br/>Loading file...';
-    $salsify->load_data($file);
+    $salsify->load_data(BP.DS.'var'.DS.'salsify'.DS.'export.json');
 
     echo '<br/>Data loaded!';
   }
