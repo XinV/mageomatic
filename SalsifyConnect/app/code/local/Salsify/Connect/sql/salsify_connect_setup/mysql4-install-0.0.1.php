080fdccf1f8ce14f9bd408b8e74e4aac2f960ec7
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-11 12:03:01 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/sql/salsify_connect_setup/mysql4-install-0.0.1.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/sql/salsify_connect_setup/mysql4-install-0.0.1.php
@@ -40,21 +40,21 @@ $table = $installer->getConnection()->newTable($installer->getTable(
   ->addColumn('start_time', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
     'nullable' => false,
     ), 'Salsify Connect Import Run Start Time')
-  // ->addColumn('end_time', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
+  // ->addColumn('end_time', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
   //   'nullable' => true,
   //   ), 'Salsify Connect Import Run End Time')
-  // ->addColumn('configuration_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-  //   'unsigned' => true,
-  //   'nullable' => false,
-  //   ), 'Salsify Connect Import ID')
-  // ->addForeignKey(
-  //         $installer->getFkName('salsify_connect/import_run', 'configuration_id',
-  //                               'salsify_connect/configuration', 'id'),
-  //         'configuration_id',
-  //         $installer->getTable('salsify_connect/configuration'),
-  //         'id',
-  //         Varien_Db_Ddl_Table::ACTION_NO_ACTION,
-  //         Varien_Db_Ddl_Table::ACTION_NO_ACTION)
+  ->addColumn('configuration_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+    'unsigned' => true,
+    'nullable' => false,
+    ), 'Salsify Connect Import ID')
+  ->addForeignKey(
+          $installer->getFkName('salsify_connect/import_run', 'configuration_id',
+                                'salsify_connect/configuration', 'id'),
+          'configuration_id',
+          $installer->getTable('salsify_connect/configuration'),
+          'id',
+          Varien_Db_Ddl_Table::ACTION_NO_ACTION,
+          Varien_Db_Ddl_Table::ACTION_NO_ACTION)
   ->setComment('Salsify_Connect salsify_connect/import_run entity table');
 $installer->getConnection()->createTable($table);
 
