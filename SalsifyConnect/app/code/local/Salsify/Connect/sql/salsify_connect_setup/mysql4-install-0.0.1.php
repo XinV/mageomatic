70725d16ee249f363a6fa5c29b4d067d53dc7d35
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-11 13:16:12 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/sql/salsify_connect_setup/mysql4-install-0.0.1.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/sql/salsify_connect_setup/mysql4-install-0.0.1.php
@@ -28,6 +28,10 @@ $table = $installer->getConnection()->newTable($installer->getTable(
     'primary' => true,
     'identity' => true,
     ), 'Salsify Connect Import ID')
+  ->addColumn('configuration', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+    'unsigned' => true,
+    'nullable' => false,
+    ), 'Salsify Connect Import ID')
   ->addColumn('token', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
     'nullable' => false,
     ), 'Salsify Connect Import Token (provided by Salsify Server)')
@@ -43,10 +47,6 @@ $table = $installer->getConnection()->newTable($installer->getTable(
   // ->addColumn('end_time', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(
   //   'nullable' => true,
   //   ), 'Salsify Connect Import Run End Time')
-  ->addColumn('configuration', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-    'unsigned' => true,
-    'nullable' => false,
-    ), 'Salsify Connect Import ID')
   // Magento seems to have a bug here with insert statement generation.
   // ->addForeignKey(
   //         $installer->getFkName('salsify_connect/import_run', 'configuration',
