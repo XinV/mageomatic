2bddd578c39ee196ee6bfd48885fcaf33bf588a9
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-13 09:51:25 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Model/DownloadJob.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Model/DownloadJob.php
@@ -10,34 +10,34 @@ class Salsify_Connect_Model_DownloadJob extends Jowens_JobQueue_Model_Job_Abstra
       true
     );
 
-    // $url = $this->getUrl();
-    // $filename = $this->getFilename();
-    // $import_run_id = $this->getImportRunId();
+    $url = $this->getUrl();
+    $filename = $this->getFilename();
+    $import_run_id = $this->getImportRunId();
 
-    // Mage::log(
-    //   "DownloadJob#perform: (url, filename, import_run_id) = (".$url.",".$filename.",".$import_run_id.")",
-    //   null, 
-    //   'salsify.log',
-    //   true
-    // );
+    Mage::log(
+      "DownloadJob#perform: (url, filename, import_run_id) = (".$url.",".$filename.",".$import_run_id.")",
+      null, 
+      'salsify.log',
+      true
+    );
 
-    // if (!($url && $filename && $import_run_id)) {
-    //   throw new Exception("Must set url, filename, and import run id for download job.");
-    // }
+    if (!($url && $filename && $import_run_id)) {
+      throw new Exception("Must set url, filename, and import run id for download job.");
+    }
 
-    // $import = Mage::getModel('salsify_connect/importrun');
-    // $import->load($import_run_id);
-    // if (!$import->getId()) {
-    //   throw new Exception("Import run id given does not refer to a valid import run: " . $import_run_id);
-    // }
+    $import = Mage::getModel('salsify_connect/importrun');
+    $import->load($import_run_id);
+    if (!$import->getId()) {
+      throw new Exception("Import run id given does not refer to a valid import run: " . $import_run_id);
+    }
 
-    // $import->set_download_started();
-    // try {
-    //   $filename = $this->_download($url, $filename);
-    // } catch (Exception $e) {
-    //   $import->set_error($e);
-    // }
-    // $import->set_download_complete($filename);
+    $import->set_download_started();
+    try {
+      $filename = $this->_download($url, $filename);
+    } catch (Exception $e) {
+      $import->set_error($e);
+    }
+    $import->set_download_complete($filename);
   }
 
   private function _download($url, $filename) {
