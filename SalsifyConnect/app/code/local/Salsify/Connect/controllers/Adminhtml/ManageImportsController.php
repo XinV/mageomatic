abbb1da018631567bef771a1a17ec21ddd1cba01
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-14 12:14:19 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
@@ -5,6 +5,8 @@ class Salsify_Connect_Adminhtml_ManageImportsController extends Mage_Adminhtml_C
   public function indexAction() {
     Mage::log("index", null, 'salsify.log', true);
 
+    echo "HERE";
+
     $this->loadLayout();
     $this->renderLayout();
   }
