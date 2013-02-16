91d52ba270898dd70396a9721578ec5e57674798
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-15 21:23:12 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Helper/Loader.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Helper/Loader.php
@@ -380,10 +380,10 @@ class Salsify_Connect_Helper_Loader extends Mage_Core_Helper_Abstract implements
       'is_visible_in_advanced_search' => 0,
       'is_comparable' => 0,
       'is_used_for_price_rules' => 0,
-      'is_wysiwyg_enabled' => 0,
+      'is_wysiwyg_enabled' => 1,
       'is_html_allowed_on_front' => 1,
-      'is_visible_on_front' => 0,
-      'used_in_product_listing' => 0,
+      'is_visible_on_front' => 1,
+      'used_in_product_listing' => 1,
       'used_for_sort_by' => 0,
       'type' => $attribute_type,
       'frontend_input' => $attribute_type, //'boolean','text', etc.
