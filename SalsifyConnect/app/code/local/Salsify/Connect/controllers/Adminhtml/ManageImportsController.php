59de0b450df07bdb9dc6d9c6247a37c746de1b4a
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-19 14:48:16 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
@@ -118,8 +118,8 @@ class Salsify_Connect_Adminhtml_ManageImportsController extends Mage_Adminhtml_C
 
   private function _create_category() {
     $category = new Mage_Catalog_Model_Category();
-    $ids = $category->_db->fetchCol("SELECT DISTINCT entity_id FROM catalog_category_entity_varchar WHERE value LIKE 'Joel%'");
-    $this->_log("IDS: ". var_export($ids, true));
+    // $ids = $category->_db->fetchCol("SELECT DISTINCT entity_id FROM catalog_category_entity_varchar WHERE value LIKE 'Joel%'");
+    // $this->_log("IDS: ". var_export($ids, true));
     
     // TODO we're currently ignoring this. I *think* this sets the default store.
     // $category->setStoreId(0);
