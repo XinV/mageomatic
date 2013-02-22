cdc27deac18c9edabc49bc4f618f67bf70f85ff3
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-22 10:31:36 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
@@ -116,7 +116,7 @@ class Salsify_Connect_Adminhtml_ManageImportsController extends Mage_Adminhtml_C
     $this->_start_render('salsify_connect_menu/testload');
     
     $salsify = Mage::helper('salsify_connect');
-    $file = BP.DS.'var'.DS.'salsify'.DS.'simple3.json';
+    $file = BP.DS.'var'.DS.'salsify'.DS.'simple.json';
     $salsify->load_data($file);
 
     $this->_render_html("<h1>Import Succeeded</h1>");
