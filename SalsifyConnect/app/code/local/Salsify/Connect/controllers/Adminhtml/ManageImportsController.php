ce6d2ce39e886f397d87269318aa81b8649e16df
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-22 13:51:10 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
@@ -65,29 +65,29 @@ class Salsify_Connect_Adminhtml_ManageImportsController extends Mage_Adminhtml_C
   public function testAction() {
     $this->_start_render('salsify_connect_menu/test');
 
-    $sql = "START TRANSACTION;
-    DROP TABLE IF EXISTS `catalog_category_entity_tmp`;
-    CREATE TABLE catalog_category_entity_tmp LIKE catalog_category_entity;
-    INSERT INTO catalog_category_entity_tmp SELECT * FROM catalog_category_entity;
+    // $sql = "START TRANSACTION;
+    // DROP TABLE IF EXISTS `catalog_category_entity_tmp`;
+    // CREATE TABLE catalog_category_entity_tmp LIKE catalog_category_entity;
+    // INSERT INTO catalog_category_entity_tmp SELECT * FROM catalog_category_entity;
 
-    UPDATE catalog_category_entity cce
-    SET children_count =
-    (
-        SELECT count(cce2.entity_id) - 1 as children_county
-        FROM catalog_category_entity_tmp cce2
-        WHERE PATH LIKE CONCAT(cce.path,'%')
-    );
+    // UPDATE catalog_category_entity cce
+    // SET children_count =
+    // (
+    //     SELECT count(cce2.entity_id) - 1 as children_county
+    //     FROM catalog_category_entity_tmp cce2
+    //     WHERE PATH LIKE CONCAT(cce.path,'%')
+    // );
 
-    DROP TABLE catalog_category_entity_tmp;
-    COMMIT;";
-    try {
-      $db = Mage::getSingleton('core/resource')
-                ->getConnection('core_write');
-      $db->query($sql);
-    } catch (Exception $e) {
-      $this->_log("FAIL: " . $e->getMessage());
-      throw $e;
-    }
+    // DROP TABLE catalog_category_entity_tmp;
+    // COMMIT;";
+    // try {
+    //   $db = Mage::getSingleton('core/resource')
+    //             ->getConnection('core_write');
+    //   $db->query($sql);
+    // } catch (Exception $e) {
+    //   $this->_log("FAIL: " . $e->getMessage());
+    //   throw $e;
+    // }
 
     $this->_end_render();
   }
