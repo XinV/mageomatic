8eebb49764c37d3d97202b8b57e2eab237984543
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-02-22 17:09:49 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Model/ImportJob.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Model/ImportJob.php
@@ -111,6 +111,7 @@ class Salsify_Connect_Model_ImportJob extends Mage_Core_Model_Abstract {
           $gallery_data = $product->getMediaGallery();
           $this->_log("GALLERY: " . var_export($gallery_data, true));
           foreach ($gallery_data['images'] as $image) {
+            $this->_log("FILE I FOUND: " . $filename);
             $this->_log("IMAGE: " . var_export($image, true));
             // if ($gallery->getBackend()->getImage($product, $image['file'])) {
             //   $gallery->getBackend()->removeImage($product, $image['file']);
