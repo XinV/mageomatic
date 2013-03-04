7c7524041352e18024df2aa85bebfa90c5173c9c
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-04 13:11:55 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Helper/SalsifyAPI.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Helper/SalsifyAPI.php
@@ -64,12 +64,12 @@ class Salsify_Connect_Helper_SalsifyAPI extends Mage_Core_Helper_Abstract {
 
     // TODO I don't agree with this serialization format
     $body = json_decode($mes->getBody(), true);
-    $this->_log("BODY: " . var_export($body));
     $url_structure = $body['url'];
     $url  = $url_structure['scheme'] . '://' . $url_structure['host'] . ':' .
             $url_structure['port'] . $url_structure['path'] .
             '?' . $url_structure['query'];
     $body['url'] = $url;
+    $this->_log("BODY: " . var_export($body));
     return $body;
   }
 
