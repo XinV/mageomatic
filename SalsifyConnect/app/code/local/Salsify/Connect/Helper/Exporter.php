49e73898985b6875b19f0ea00e863464e4a65fa5
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-06 17:30:34 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Helper/Exporter.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Helper/Exporter.php
@@ -247,7 +247,10 @@ self::_log("1");
       $parent = Mage::getModel('catalog/category')
                     ->getCollection()
                     ->load($parent_id);
-self::_log("2: " . var_export($parent,true));
+      if (!$parent) {
+        self::_log("FUCK");
+      }
+self::_log("2: " . var_export($parent, true));
       $this->_load_category_mapping($parent);
 self::_log("3");
     }
