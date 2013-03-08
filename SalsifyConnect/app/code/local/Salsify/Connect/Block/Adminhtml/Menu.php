f6a558f7e482a4e7b17742ee35b3e1c046319095
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-08 11:36:21 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Menu.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Menu.php
@@ -1,10 +1,11 @@
 <?php
 
 // Block for rendering the Salsify admin menu
-class Salsify_Connect_Block_Adminhtml_Menu extends Mage_Core_Block_Template {
+// class Salsify_Connect_Block_Adminhtml_Menu extends Mage_Core_Block_Template {
+class Salsify_Connect_Block_Adminhtml_Menu extends Mage_Core_Block_Abstract {
 
   public function _construct() {
-    $this->setTemplate('nofrills_helloworld.phtml');
+    // $this->setTemplate('nofrills_helloworld.phtml');
     return parent::_construct();
   }
 
