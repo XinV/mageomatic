1611193527597c8ed036d1ba7b00061b71852463
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-08 15:18:12 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Admin.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Admin.php
@@ -1,28 +1,14 @@
 <?php
 
 // Primary rendering block
-class Salsify_Connect_Block_Adminhtml_Admin extends Mage_Core_Block_Abstract {
-
-  private static function _log($msg) {
-    Mage::log('Block_Adminhtml_Admin: ' . $msg, null, 'salsify.log', true);
-  }
-
-  public function _construct() {
-    self::_log("THERE " . var_export($this->_layout, true));
-
-    // $menu_block = $this->_layout
-    //                    ->createBlock('salsify_connect/adminhtml_menu','sidebar');
-    // $this->setChild($menu_block, 'salsify-menu');
-    return parent::_construct();
-  }
+class Salsify_Connect_Block_Adminhtml_Admin extends Mage_Core_Block_Template {
 
   public function _beforeToHtml() {
-    self::_log("HERE");
-
     $menu_block = $this->_layout
                        ->createBlock('salsify_connect/adminhtml_menu','sidebar');
     $this->setChild($menu_block, 'salsify-menu');
-    // return parent::_construct();
+
+    // TODO get the other blocks to show up here
   }
 
 }
