3f845ff0d2b6e9c43213882b2626eb1133ae747d
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-08 11:38:12 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Menu.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Menu.php
@@ -4,9 +4,14 @@
 // class Salsify_Connect_Block_Adminhtml_Menu extends Mage_Core_Block_Template {
 class Salsify_Connect_Block_Adminhtml_Menu extends Mage_Core_Block_Abstract {
 
-  public function __construct() {
+  public function _construct() {
     // $this->setTemplate('nofrills_helloworld.phtml');
-    return parent::__construct();
+    return parent::_construct();
+  }
+
+  public function toHtml() {
+    // FIXME
+    return "HERE";
   }
 
 }
