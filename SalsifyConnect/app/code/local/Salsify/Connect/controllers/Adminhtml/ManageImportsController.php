c0387dc2089313de64f045ebe2bf77283a04b873
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-08 11:42:00 -0500

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/controllers/Adminhtml/ManageImportsController.php
@@ -66,8 +66,8 @@ class Salsify_Connect_Adminhtml_ManageImportsController extends Mage_Adminhtml_C
   public function testAction() {
     $this->_start_render('salsify_connect_menu/test');
 
-    // $block = new Salsify_Connect_Block_Adminhtml_Menu();
-    // $this->_addContent($block);
+    $this->_render_html('<h1>Trying to render custom block</h1>');
+
     $block = $this->getLayout()
                   ->createBlock('salsify_connect/adminhtml_menu');
     $this->_addContent($block);
