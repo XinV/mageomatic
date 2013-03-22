8e60b5fffabfdab7a4c29f49d61b27f1afe73092
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-22 15:27:47 -0400

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Model/Resource/Setup.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Model/Resource/Setup.php
@@ -1,5 +1,4 @@
 <?php
-class Salsify_Connect_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
-{
+class Salsify_Connect_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup {
   // This space intentionally left blank.
 }
