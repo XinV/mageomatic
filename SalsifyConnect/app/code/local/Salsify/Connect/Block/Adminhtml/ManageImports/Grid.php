9f07cfb9d731bca36c1cf9c6b2060b35b8a44a80
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-22 16:13:46 -0400

    dev

---
+++ SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/ManageImports/Grid.php
@@ -0,0 +1,52 @@
+<?php
+class Foo_Bar_Block_Adminhtml_ManageImports_Grid extends Mage_Adminhtml_Block_Widget_Grid {
+
+  public function __construct() {
+    parent::__construct();
+     
+    // Set some defaults for our grid
+    $this->setDefaultSort('id');
+    $this->setId('salsify_connect_manageimports_grid');
+    $this->setDefaultDir('desc');
+    $this->setSaveParametersInSession(true);
+  }
+
+  protected function _getCollectionClass() {
+    // This is the model we are using for the grid
+    return 'salsify_connect/importrun_collection';
+  }
+
+  protected function _prepareCollection() {
+    // Get and set our collection for the grid
+    $collection = Mage::getResourceModel($this->_getCollectionClass());
+    $this->setCollection($collection);
+
+    return parent::_prepareCollection();
+  }
+
+  protected function _prepareColumns() {
+    // Add the columns that should appear in the grid
+    $this->addColumn('id',
+      array(
+        'header'=> $this->__('ID'),
+        'align' =>'right',
+        'width' => '50px',
+        'index' => 'id'
+      )
+    );
+
+    $this->addColumn('status',
+      array(
+        'header'=> $this->__('Status'),
+        'index' => 'status'
+      )
+    );
+
+    return parent::_prepareColumns();
+  }
+
+  public function getRowUrl($row) {
+    // This is where our row data will link to
+    return $this->getUrl('*/*/edit', array('id' => $row->getId()));
+  }
+}
