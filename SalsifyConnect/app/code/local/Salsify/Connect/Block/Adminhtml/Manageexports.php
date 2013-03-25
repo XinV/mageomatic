4b8ffbe58945b7ec32c371f3027a2f81ceb782be
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-25 12:18:29 -0400

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Manageexports.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Block/Adminhtml/Manageexports.php
@@ -9,10 +9,9 @@ class Salsify_Connect_Block_Adminhtml_Manageexports extends Mage_Adminhtml_Block
     $this->_blockGroup = 'salsify_connect';
     $this->_controller = 'adminhtml_manageexports';
     $this->_headerText = $this->__('Manage Exports to Salsify');
+    parent::__construct();
 
     $this->_addButtonLabel = 'Create New Import';
-    
-    parent::__construct();
 
     // remove the Add New button
     $this->removeButton('add');
