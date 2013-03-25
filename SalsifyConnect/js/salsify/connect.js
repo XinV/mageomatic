e4a279b93e8416ac2a209ae7bee1a53e6417305e
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-25 13:18:24 -0400

    dev

--- SalsifyConnect/js/salsify/connect.js
+++ SalsifyConnect/js/salsify/connect.js
@@ -1,6 +1,6 @@
 var salsify = salsify || {};
 salsify = (function (parent) {
-  var sc = parent.connect || parent.connect {};
+  var sc = parent.connect = parent.connect || {};
 
   sc.createExport = function () {
     alert("IT WORKS");
