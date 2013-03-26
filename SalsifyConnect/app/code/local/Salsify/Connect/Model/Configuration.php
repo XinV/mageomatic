156608037e98e8e152aab39bd56673d0d98b30b9
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-26 12:54:31 -0400

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Model/Configuration.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Model/Configuration.php
@@ -8,7 +8,7 @@
 class Salsify_Connect_Model_Configuration extends Mage_Core_Model_Abstract {
 
   private static function _log($msg) {
-    Mage::log(get_called_class()': ' . $msg, null, 'salsify.log', true);
+    Mage::log(get_called_class() . ': ' . $msg, null, 'salsify.log', true);
   }
 
   protected function _construct() {
