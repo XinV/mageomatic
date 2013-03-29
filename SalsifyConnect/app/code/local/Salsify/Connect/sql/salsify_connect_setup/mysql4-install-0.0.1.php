08638cfae183e77329809f3217e506f10972fca3
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-03-29 14:48:18 -0400

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/sql/salsify_connect_setup/mysql4-install-0.0.1.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/sql/salsify_connect_setup/mysql4-install-0.0.1.php
@@ -103,7 +103,7 @@ $table = $installer->getConnection()->newTable($installer->getTable(
   ->addColumn('magento_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
     'unsigned' => true,
     'nullable' => false,
-    ), 'ID of Accessory Type in Magento (enum)')
+    ), 'ID of Accessory Type in Magento (enum)');
 $installer->getConnection()->createTable($table);
 
 
