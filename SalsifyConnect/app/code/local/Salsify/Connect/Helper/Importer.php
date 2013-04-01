d09d6a4982d96d7c1934abac3562f0b186cbc7fb
Rob Gonzalez <rob.gonzalez@gmail.com>
2013-04-01 16:24:42 -0400

    dev

--- SalsifyConnect/app/code/local/Salsify/Connect/Helper/Importer.php
+++ SalsifyConnect/app/code/local/Salsify/Connect/Helper/Importer.php
@@ -579,8 +579,7 @@ class Salsify_Connect_Helper_Importer extends Mage_Core_Helper_Abstract implemen
 
           // make sure to skip attributes that are owned by Magento
           if (!$mapper::isAttributeMagentoOwned($code)) {
-            // FIXME need datatype
-            $this->_log("ATTRIBUTE: " . var_export($attribute,true));
+            $value = $mapper::castValueByBackendType($value, $attribute['__backend_type']);
             $this->_product[$code] = $value;
           }
         } else {
